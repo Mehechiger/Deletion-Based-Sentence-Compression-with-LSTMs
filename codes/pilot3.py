006c103a4a00d514e65cb93abce9615d0c93fe0b
Mehechiger <Mehechiger@users.noreply.github.com>
2020-04-03 00:25:21 +0200

    orig vocab minfreq = 2

--- codes/pilot3.py
+++ codes/pilot3.py
@@ -123,7 +123,7 @@ vectors_cache = "/Users/mehec/Google Drive/vector_cache" \
     if not os.path.isdir("/content/")\
     else "/content/drive/My Drive/vector_cache"
 ORIG.build_vocab(train,
-                 min_freq=1,
+                 min_freq=2,
                  vectors="glove.6B.300d",
                  vectors_cache=vectors_cache
                  )
