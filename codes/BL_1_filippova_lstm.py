253dc8e1006c06cf5430eff20b8a0d14efbd0b9f
Mehechiger <Mehechiger@users.noreply.github.com>
2020-05-20 14:56:30 +0200

    corrected beam sort order (sorted(reverse=True))

--- codes/BL_1_filippova_lstm.py
+++ codes/BL_1_filippova_lstm.py
@@ -152,9 +152,9 @@ give_label(test)
 """
 """
 # for testing use only small amount of data
-train, _ = train.split(split_ratio=0.01)
-val, _ = val.split(split_ratio=0.01)
-test, _ = test.split(split_ratio=0.01)
+train, _ = train.split(split_ratio=0.001)
+val, _ = val.split(split_ratio=0.001)
+test, _ = test.split(split_ratio=0.001)
 #test, _ = train.split(split_ratio=0.1)
 #val = test = train
 
@@ -340,8 +340,8 @@ class Seq2Seq(nn.Module):
                                           prob+output_tops[0][:, i],
                                           labels+[output, ]
                                           ))
-                beam = sorted(next_beam, key=lambda x: x[3].mean())[:n]
-            labels = sorted(beam, key=lambda x: x[3].mean())[0][4]
+                beam = sorted(next_beam, key=lambda x: x[3].mean(), reverse=True)[:n]
+            labels = sorted(beam, key=lambda x: x[3].mean(), reverse=True)[0][4]
             for i in range(len(labels)):
                 outputs[i] = labels[i]
         return outputs
