b910687c52702bc64ce9971f254cd40e68ec46c0
Mehechiger <Mehechiger@users.noreply.github.com>
2020-05-26 00:07:54 +0200

    batch size 128
    accum steps 1
    train at 0.01 total train

--- codes/B_1_BL_1_filippova_lstm.py
+++ codes/B_1_BL_1_filippova_lstm.py
@@ -157,7 +157,7 @@ give_label(test)
 """
 """
 # for testing use only small amount of data
-train, _ = train.split(split_ratio=0.05)
+train, _ = train.split(split_ratio=0.01)
 val, _ = val.split(split_ratio=0.005)
 test, _ = test.split(split_ratio=0.005)
 # test, _ = train.split(split_ratio=0.1)
