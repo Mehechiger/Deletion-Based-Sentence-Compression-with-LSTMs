f684bf8c815fca3551e66e654d97e2d84c48c6f0
Mehechiger <Mehechiger@users.noreply.github.com>
2020-05-31 19:51:17 +0200

    full train val test

--- codes/B_1_BL_1_filippova_lstm.py
+++ codes/B_1_BL_1_filippova_lstm.py
@@ -171,14 +171,14 @@ ORIG.build_vocab(train, min_freq=1, vectors="glove.840B.300d", vectors_cache=VEC
 COMPR.build_vocab(train, min_freq=1)
 
 """
-"""
 # for testing use only small amount of data
-#train, _ = train.split(split_ratio=0.0001)
-val, _ = val.split(split_ratio=0.05)
+train, _ = train.split(split_ratio=0.0001)
+val, _ = val.split(split_ratio=0.005)
 # _, val = train.split(split_ratio=0.9995)
-test, _ = test.split(split_ratio=0.05)
+test, _ = test.split(split_ratio=0.005)
 # test, _ = train.split(split_ratio=0.1)
 # val = test = train
+"""
 """
 """
 
