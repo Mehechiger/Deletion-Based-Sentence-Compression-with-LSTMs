02c6259aa69db816f893b6288bc91a49d5d0afa1
Mehechiger <Mehechiger@users.noreply.github.com>
2020-06-01 13:27:15 +0200

    reverted freeze glove emb for both

--- codes/B_1_BL_1_filippova_lstm.py
+++ codes/B_1_BL_1_filippova_lstm.py
@@ -179,10 +179,10 @@ COMPR.build_vocab(train, min_freq=1)
 """
 """
 # for testing use only small amount of data
-#train, _ = train.split(split_ratio=0.0001)
-val, _ = val.split(split_ratio=0.05)
+train, _ = train.split(split_ratio=0.0001)
+val, _ = val.split(split_ratio=0.005)
 # _, val = train.split(split_ratio=0.9995)
-test, _ = test.split(split_ratio=0.05)
+test, _ = test.split(split_ratio=0.005)
 # test, _ = train.split(split_ratio=0.1)
 # val = test = train
 """
@@ -239,7 +239,7 @@ class Encoder(nn.Module):
         self.n_layers = n_layers
 
         # self.embedding = nn.Embedding(input_dim, emb_dim)
-        self.embedding = nn.Embedding.from_pretrained(pretrained_vectors, freeze=True)
+        self.embedding = nn.Embedding.from_pretrained(pretrained_vectors)
         # self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
         self.rnn = nn.LSTM(self.emb_dim, hid_dim, n_layers, dropout=dropout)
 
@@ -263,7 +263,7 @@ class Decoder(nn.Module):
         self.device = device
 
         # self.embedding_src = nn.Embedding(input_dim, emb_src_dim)
-        self.embedding_src = nn.Embedding.from_pretrained(pretrained_vectors, freeze=True)
+        self.embedding_src = nn.Embedding.from_pretrained(pretrained_vectors)
         self.embedding_input = lambda l: torch.eye(
             emb_input_dim)[l.view(-1)].unsqueeze(0).to(self.device)
         self.rnn = nn.LSTM(self.emb_src_dim + emb_input_dim,
