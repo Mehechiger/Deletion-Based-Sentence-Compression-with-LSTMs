cf3896f383499137f24420fea339cf208492aed7
Mehechiger <Mehechiger@users.noreply.github.com>
2020-06-19 12:54:29 +0200

    train greedy, eval beam, with inepochs, full size (val test at 0.05)

--- codes/B_1_BL_1_filippova_lstm.py
+++ codes/B_1_BL_1_filippova_lstm.py
@@ -194,14 +194,14 @@ ORIG.build_vocab(train, min_freq=1, vectors="glove.840B.300d", vectors_cache=VEC
 COMPR.build_vocab(train, min_freq=1)
 
 """
+"""
 # for testing use only small amount of data
-train, _ = train.split(split_ratio=0.01)
+#train, _ = train.split(split_ratio=0.01)
 val, _ = val.split(split_ratio=0.05)
 # _, val = train.split(split_ratio=0.9995)
 test, _ = test.split(split_ratio=0.05)
 # test, _ = train.split(split_ratio=0.1)
 # val = test = train
-"""
 """
 """
 
