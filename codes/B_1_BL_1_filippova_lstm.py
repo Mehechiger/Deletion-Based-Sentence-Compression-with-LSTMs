45168f6b64ae12993f47df12671c02fc5366c2c3
Mehechiger <Mehechiger@users.noreply.github.com>
2020-06-19 18:15:48 +0200

    d

--- codes/B_1_BL_1_filippova_lstm.py
+++ codes/B_1_BL_1_filippova_lstm.py
@@ -198,7 +198,6 @@ DEP.build_vocab(train, min_freq=1)
 COMPR.build_vocab(train, min_freq=1)
 
 """
-"""
 # for testing use only small amount of data
 train, _ = train.split(split_ratio=0.0001)
 val, _ = val.split(split_ratio=0.05)
@@ -206,6 +205,7 @@ val, _ = val.split(split_ratio=0.05)
 test, _ = test.split(split_ratio=0.05)
 # test, _ = train.split(split_ratio=0.1)
 # val = test = train
+"""
 """
 """
 
